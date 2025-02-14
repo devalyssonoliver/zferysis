4846fa5b7bdf3e22f979a05cf8b2f03d7529de31
devalyssonoliver <alyzsorn@gmail.com>
2025-02-14 17:22:40 -0300

    refatorando a view principal utilizando devexpress

---
+++ Views/frmPrincipalDevExpress.pas
@@ -0,0 +1,37 @@
+unit frmPrincipalDevExpress;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, cxGraphics, dxUIAClasses, cxControls,
+  cxLookAndFeels, cxLookAndFeelPainters, dxRibbonCustomizationForm, dxCore,
+  dxRibbonSkins, cxClasses, dxRibbon, cxTextEdit, cxContainer, cxEdit,
+  dxStatusBar, dxRibbonStatusBar, cxLabel, dxGallery, dxGalleryControl,
+  dxRibbonBackstageViewGalleryControl, dxRibbonBackstageView, dxBar,
+  cxBarEditItem, Vcl.Buttons;
+
+type
+  TForm2 = class(TForm)
+    dxbrManager1: TdxBarManager;
+    dxBarManager1Bar: TdxBar;
+    dxBarManager1Bar2: TdxBar;
+    cxbarItem: TcxBarEditItem;
+    dxrbn1: TdxRibbon;
+    dxrbntbCadastros: TdxRibbonTab;
+    dxrbnstsbr1: TdxRibbonStatusBar;
+    btnFormUser: TSpeedButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
